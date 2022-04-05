ca01bbd7497ee2304b86af1d8d3386a37b3308fb
luiz <luizes_@hotmail.com>
2022-04-05 19:58:29 -0300

    userResource

---
+++ src/main/java/com/luizfernandes/workshopmongo/domain/Comment.java
@@ -0,0 +1,9 @@
+package com.luizfernandes.workshopmongo.domain;
+
+import java.util.Date;
+
+public class Comment {
+	private String idString;
+	private String text;
+	private Date date;
+}
---
+++ src/main/java/com/luizfernandes/workshopmongo/domain/Post.java
@@ -0,0 +1,11 @@
+package com.luizfernandes.workshopmongo.domain;
+
+import java.util.Date;
+
+public class Post {
+	private String id;
+	private Date date;
+	private String title;
+	private String corpo;
+
+}

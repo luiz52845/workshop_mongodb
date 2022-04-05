35ceff34070d5a36b5789565e33510d6cf266be6
luiz <luizes_@hotmail.com>
2022-04-05 20:49:55 -0300

    config instantiaon

---
+++ src/main/java/com/luizfernandes/workshopmongo/config/Instatiation.java
@@ -0,0 +1,31 @@
+package com.luizfernandes.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.luizfernandes.workshopmongo.domain.User;
+import com.luizfernandes.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instatiation implements CommandLineRunner {
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
